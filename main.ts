dec3194b4b68161dc7f655e0212ec82fdb7f58f1
dannytran14 <81594920+dannytran14@users.noreply.github.com>
2021-05-19 12:38:12 +1000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,3 @@
 basic.forever(function () {
-    MTS_RVR.Tag_Tracking_Mode()
-    while (!(MTS_RVR.H_Is_Home_Located())) {
-        MTS_RVR.Turn(20)
-    }
-    MTS_RVR.H_Centre_Object_On_Screen()
-    while (!(MTS_RVR.S_Is_Object_Detected())) {
-        MTS_RVR.Move(30)
-    }
-    MTS_RVR.Stop()
-})
-basic.forever(function () {
 	
 })
