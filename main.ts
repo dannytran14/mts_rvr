8f7f8116c18ceeb9af8d1c4bd0a4123ce174aa7b
dannytran14 <81594920+dannytran14@users.noreply.github.com>
2022-02-11 14:02:11 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+input.onButtonPressed(Button.A, function () {
+    MTS_RVR.Start_Up()
+    MTS_RVR.Move(5)
+    MTS_RVR.Turn_Right(90)
+    MTS_RVR.Move(5)
+    MTS_RVR.Turn_Right(90)
+    MTS_RVR.Move(5)
+})
